0b3ecda93de47d191ec08bbc3dc9b39d5d31c506
LatfullinAS <LatfullinAS@transport.mos.ru>
2023-12-09 17:17:18 +0300

    add

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <gis-map
+      :latitude="55.751834296843505"
+      :longitude="37.61911466717721"
+      :propZoom="12"
+    ></gis-map>
+  </v-app>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+import GisMap from "./components/GisMap.vue";
+
+export default {
+  name: "app",
+  components: {
+    GisMap,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/GisMap.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="map" style="width: 100%; height: 100%">
+    <l-map
+      v-if="ready"
+      ref="map"
+      v-model:zoom="zoom"
+      :center="[latitude, longitude]"
+    >
+      <l-geo-json :geojson="geoJSON" :options="geojsonOptions" />
+    </l-map>
+  </div>
+</template>
+
+<script lang="ts">
+import "2gis-maps";
+import axios from "axios";
+import { Vue, Options } from "vue-class-component";
+import { LMap, LGeoJson } from "@vue-leaflet/vue-leaflet";
+
+@Options({
+  components: {
+    LMap,
+    LGeoJson,
+  },
+  props: {
+    latitude: Number,
+    longitude: Number,
+    propZoom: Number,
+  },
+  beforeMount() {
+    this.getURLFragment().then({});
+    this.zoom = this.propZoom;
+  },
+  methods: {
+    getURLFragment() {
+      return axios
+        .get(decodeURIComponent(window.location.hash.substr(1)))
+        .then((response) => {
+          console.log(response.data);
+          this.geoJSON = response.data;
+          this.ready = true;
+        })
+        .catch((err) => {
+          console.error(err);
+          this.geoJSON = {};
+          this.ready = true;
+        });
+    },
+  },
+})
+export default class GisMap extends Vue {
+  zoom!: number;
+  latitude!: number;
+  longitude!: number;
+  geoJSON!: any;
+  ready: boolean = false;
+  geojsonOptions: any = {
+    onEachFeature: (f: any, l: any) => {
+      l.bindPopup(
+        '<pre style="height: 100px; overflow: scroll;">' +
+          JSON.stringify(f.properties, null, " ").replace(
+            //eslint-disable-next-line
+            /[\{\}"]/g,
+            ""
+          ) +
+          "</pre>"
+      );
+    },
+  };
+}
+</script>
